9741068a0a4a9e00b6d8d7ca335cdde4f51b65f5
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2022-08-08 22:51:11 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -11,7 +11,37 @@ for (pagename,worddict) in fulldict.items():
             english=worddict["en"]
         else:#如果没有英文，则用页面名称
             english=pagename.split("#")[0]
-        #print(pagename)
+        #(排序索引（拼音），行)
         dictlist.append((unidecode.unidecode(worddict["zhs_rm"]).lower(),"- **{}** {}".format(worddict["zhs"],english)))
 dictlist.sort(key=(lambda x:x[0]))
-open("zh_en.md","w",encoding="utf8").write("# 原神汉英词典\n# Genshin Impact Chinese-English Dictionary\n收录游戏《原神》中，官方名词的中文及英文翻译\n\n内容来自[Genshin Impact Wiki](https://genshin-impact.fandom.com/)，在CC-BY-SA协议下提供。\n\n可使用Ctrl+F在本页面查找。\n\n"+"\n".join([x[-1] for x in dictlist]))
+zh_en_description="""# 原神汉英词典
+# Genshin Impact Chinese-English Dictionary
+收录游戏《原神》中，官方名词的中文及英文翻译
+
+内容来自[Genshin Impact Wiki](https://genshin-impact.fandom.com/)，在CC-BY-SA协议下提供。
+
+可使用Ctrl+F在本页面查找。
+
+"""
+open("zh_en.md","w",encoding="utf8").write(zh_en_description+"\n".join([x[-1] for x in dictlist]))
+#en>zh
+dictlist=[(x,"## "+x.upper()) for x in "abcdefghijklmnopqrstuvwxyz"]
+for (pagename,worddict) in fulldict.items():
+    if("zhs" in worddict):
+        if("en" in worddict):
+            english=worddict["en"]
+        else:#如果没有英文，则用页面名称
+            english=pagename.split("#")[0]
+        #(排序索引，行)
+        dictlist.append((english,"- **{}** {}".format(english,worddict["zhs"])))
+dictlist.sort(key=(lambda x:x[0]))
+en_zh_description="""# 原神英汉词典
+# Genshin Impact English-Chinese Dictionary
+收录游戏《原神》中，官方名词的中文及英文翻译
+
+内容来自[Genshin Impact Wiki](https://genshin-impact.fandom.com/)，在CC-BY-SA协议下提供。
+
+可使用Ctrl+F在本页面查找。
+
+"""
+open("en_zh.md","w",encoding="utf8").write(en_zh_description+"\n".join([x[-1] for x in dictlist]))
